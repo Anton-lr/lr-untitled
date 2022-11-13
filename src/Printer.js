556f26bf150375ee99215ac24fc93f16a46fb472
Anton-lr <Anton.nydal@hotmail.com>
2022-11-13 04:29:15 +0100

    made printer functions that work for all required objects

--- src/Printer.js
+++ src/Printer.js
@@ -2,31 +2,46 @@ import { printTs } from "./frameConversion";
 
 const Printer = ({ data, setData, type }) => {
     //offset, duration, initialData, targetData¨
-
     if (typeof (data) === "undefined") {
         return ;
     }
 
-    function generateList() {
-        const rets = []
-        for (var i = 0; i < data.length; i++) {
-            const d = <div className="litem" id={i}>
-                    <button className="deleteBelow" onClick={db(i)}> delete below </button>
-                    <button className="removeItem" onClick={ri(i)}> delete below </button>
-                <p className=""> {codePrint(data[i], type)} </p>
-                </div>;
-            rets.push(d)
+    function myPrint(f, type = "v") { // use map
+        let d = f
+        if (type === "keyframes") {
+            d = printTs(d)
         }
-        return rets
+        if (type === "code") {
+            //d = "[[" + (d[0]) + "], " + d[1] + "],"
+            const json = JSON.stringify(f);  // {"name":"John Smith"}
+            const unquoted = json.replace(/"([^"]+)":/g, '$1:');
+            d = unquoted
+        }
+        return d
+
     }
 
-    function db(i) {
-        const dd = data.slice(0, i)
+    function generateList(s) {
+        const a = Array.from(Array(s.length).keys()).map(i => {
+            return(
+            <div className="litem" key={i.toString()} >
+                <button className="removeItem" onClick={() => ri(s, i)}> remove </button>
+                <button className="deleteBelow" onClick={() => db(s, i)}> slice </button>
+
+                {myPrint(s[i], type)}
+            </div>)
+    
+        })
+        return a
+    }
+
+    function db(list, i) {
+        const dd = list.slice(0, i)
         setData(dd)
     }
 
-    function ri(i) {
-        const ed = data.slice(0, i).concat(data.slice(i+1, data.length))
+    function ri(list, i) {
+        const ed = list.slice(0, i).concat(list.slice(i + 1, list.length))
         setData(ed)
     }
 
@@ -38,8 +53,7 @@ const Printer = ({ data, setData, type }) => {
         }
         return vals
     }
-    const a = generateList()
-
+    const a = generateList(data)
     return a;
 }
 
